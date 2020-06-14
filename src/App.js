4789f7802537851fa54c5c8cdbcd711797705532
Victor Giudicissi <victor.giudicissi@exactaworks.com.br>
2020-06-14 18:30:02 -0300

    Criada estrutura base do projeto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+
+import background from './resources/white-truck.png'
+import logo from './resources/caminhao-infotrecho.png'
 
 function App() {
+
+  var fatherStyle = {
+    position: "fixed",
+    width: "100%",
+    height: "100%",
+    backgroundImage: `url(${background})`,
+    backgroundSize: 'cover',
+  };
+
+  var headerStyle = {
+    marginLeft: "100px",
+    marginTop: "20px",
+    width: "100%",
+  }
+
+  var headerItem = { 
+    display: "inline",
+    marginBottom: "30px",
+    marginLeft: "10px"
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={fatherStyle}>
+      <div id="header" style={headerStyle}>
+          <img src={logo} alt="Logo" />
+          <h2 style={headerItem}>InfoTrecho</h2>
+      </div>
     </div>
   );
 }
